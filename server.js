81ffad575c239555009506ad3c05d0fbb7fb2676
Shubhamkale9922 <shubhamkale992220@gmail.com>
2026-05-20 16:03:30 +0530

    UI Changes

--- server.js
+++ server.js
@@ -149,10 +149,10 @@ app.get('/', (req, res) => {
     <!-- Footer -->
     <footer class="text-center text-gray-400 py-8 border-t border-[#4b1f2a] mt-12 bg-[#2b1118]">
 
-        <div class="text-3xl mb-3"> Fully automated CI/CD 🚀 </div>
+        <div class="text-3xl mb-3"> Fully automated CI/CD  Shubham Kale 🚀 </div>
 
         <p class="text-lg">
-            Deployed using Modern AWS DevOps Architecture And Some GPT Magic ✨ 
+            Deployed using Modern AWS DevOps Architecture And Some GPT Magic Shubham  ✨ 
         </p>
 
         <p class="mt-2 text-sm text-gray-500">
